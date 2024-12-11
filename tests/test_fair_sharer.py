fa35be0b5cc66df66aa32ab6a35e568c90801a4a
Lilith Freya Aimée <lilith.schmidl@study.hs-duesseldorf.de>
2024-12-11 08:00:50 +0100

    updated test_fair_sharer.py
    fixed import error

--- tests/test_fair_sharer.py
+++ tests/test_fair_sharer.py
@@ -1,7 +1,7 @@
 # test for fair_sharer.py
 
 import pytest
-from src.fair_sharer import tests.fair_sharer
+from src.fair_sharer import fair_sharer
 
 def test_fair_sharer_one_iteration():
     assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0], "Test case for one iteration failed"
