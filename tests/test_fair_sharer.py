65445f193109814ec905058fc2fd4441f66d39bd
Lilith Freya Aimée <lilith.schmidl@study.hs-duesseldorf.de>
2024-12-11 07:10:13 +0100

    created test_fair_sharer.py
    tests one case for one iteration and one case for multiple (5) iterations

---
+++ tests/test_fair_sharer.py
@@ -0,0 +1,10 @@
+# test for fair_sharer.py
+
+import pytest
+from src.fair_sharer import tests.fair_sharer
+
+def test_fair_sharer_one_iteration():
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0], "Test case for one iteration failed"
+
+def test_fair_sharer_multiple_iterations():
+    assert fair_sharer([0, 1000, 800, 0], 5) == [268.29, 634.3199999999999, 726.49, 170.9], "Test case for multiple iterations failed"
